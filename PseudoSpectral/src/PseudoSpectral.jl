0da01255e60d7c4686dec3007ebfa8cd4b6f970c
Hugh Coleman <h.coleman.25@abdn.ac.uk>
2026-05-11 12:19:58 +0100

    PSWIP

--- PseudoSpectral/src/PseudoSpectral.jl
+++ PseudoSpectral/src/PseudoSpectral.jl
@@ -2,20 +2,51 @@ module PseudoSpectral
 export pseudospectral_problem, x
 
 using SciMLBase: SplitODEProblem, ODEProblem, DiagonalOperator, ODEFunction, update_coefficients!, remake
-using FFTW: plan_r2r!, REDFT00, MEASURE
+using FFTW: plan_r2r!, REDFT00, MEASURE, r2rFFTWPlan
 using Symbolics: variable, @variables, Num, sparsejacobian, build_function, substitute, get_variables
 using Random:seed!
 
 const x = variable(:x) |> Num
 
-struct PseudoSpectralProblem
+struct PseudoSpectralProblem{BC}
+    species::Vector{Num},
+    reaction_params::Vector{Num},
+    diffusion_params::Vector{Num},
+    boundary_params::Vector{Num},
+    initial_params::Vector{Num},
+    problem::SplitODEProblem
+    size::Tuple{Int,Int}
+    plan::r2rFFTWPlan
+    fϕ,
+    fΔϕ,
+    ϕ:BC
+end
+
+struct LiftingFunction{T}
+    fϕ::T,
+    fΔϕ::T,
+    ϕ::Matrix{Float64}
+    Δϕ::Matrix{Float64}
+end
+
+LiftingFunction(dims) = LiftingFunction(nothing,nothing,zeros(dims), zeros(dims))
+LiftingFunction(dims    fϕ,  fΔϕ) = LiftingFunction(nothing,nothing,zeros(dims), zeros(dims))
+
+function remake(f::LiftingFunction)
+
+
+struct PseudoSpectralSolution{BC}
+    species
+    sol::ODESolution
+    plan::r2rFFTWPlan
+    ϕ::BC
 end
 """
 Construct a SplitODEProblem to solve a reaction diffusion system with reflective boundaries.
 
 Returns the SplitODEProblem with solutions in the frequency (DCT-1) domain and a FFTW plan to transform solutions back to the spatial domain.
 """
-function pseudospectral_problem(species, reaction_rates, diffusion_rates, boundary_conditions, initial_conditions, num_verts; noise=1e-4, seed=nothing, kwargs...)
+function PseudoSpectralProblem(species, reaction_rates, diffusion_rates, boundary_conditions, initial_conditions, num_verts; noise=1e-4, seed=nothing, kwargs...)
     n = num_verts
     m = length(species)
     
@@ -34,80 +65,96 @@ function pseudospectral_problem(species, reaction_rates, diffusion_rates, bounda
 
     fu0(i) = _fu0(i) + initial_noise
 
-    # Dispatch on `iszero(boundary_conditions)`
-    function dispatch_bcs(::Val{BC}) where BC
-        ## Offsets for constant, non-zero flux boundary conditions.
-        # For u′(0) = a, u′(1) = b,
-        # define ϕ as a smooth function such that ϕ′(0) = a, ϕ′(1) = b, and write v = u - ϕ.
-        # Then v′(0) = 0, v′(1) = 0, so we can solve for v using DCT-I.
+    if !iszero(boundary_conditions)
+        a,b = eachrow(boundary_conditions)
+        X = range(0.0,1.0,n)
+        ϕ = X.^2 * (b'-a')/2 + X * a'
+        Δϕ = ((b-a).*diffusion_rates)' |> collect
+        fϕ,_ = build_function(ϕ, bs; expression=Val{false})
+        fΔϕ,_ = build_function(Δϕ, ds,bs; expression=Val{false})
+    end
+    
+    R = reaction_operator(species, reaction_rates, rs, plan, Val(BC))
+    D = diffusion_operator(diffusion_rates, ds, n)
+    prob = SplitODEProblem(D, R, vec(u), Inf, nothing; kwargs...)
+
+    function make_problem(params; attempt=1, kwargs...)
+        r = Float64[params[k] for k in rs]
+        d = Float64[params[k] for k in ds]
+        b = Float64[params[k] for k in bs]
+        i = Float64[params[k] for k in is]
+
+        local ϕ, Δϕ
         if BC
-            a,b = eachrow(boundary_conditions)
-            X = range(0.0,1.0,n)
-            ϕ = X.^2 * (b'-a')/2 + X * a'
-            Δϕ = ((b-a).*diffusion_rates)' |> collect
-            fϕ,_ = build_function(ϕ, bs; expression=Val{false})
-            fΔϕ,_ = build_function(Δϕ, ds,bs; expression=Val{false})
+            ϕ = fϕ(b)
+            Δϕ = fΔϕ(d,b)
+        else
+            ϕ = Δϕ = Matrix{Float64}(undef,n,m)
         end
+
+        w = Matrix{Float64}(undef,n,m) # Allocate working memory for FFTW.
+
+
+        p = Parameters(w,r,d,ϕ,Δϕ,attempt)
+
+        local u0
+        u0 = fu0(i)
+        BC && (u0 .-= p.ϕ)
+        plan * u0
+        u0 = vec(u0)
         
-        R = reaction_operator(species, reaction_rates, rs, plan, Val(BC))
-        D = diffusion_operator(diffusion_rates, ds, n)
-        prob = SplitODEProblem(D, R, vec(u), Inf, nothing; kwargs...)
+        update_coefficients!(prob.f.f1.f, nothing, p, nothing) # Set parameter values in diffusion operator.
+        remake(prob; p, u0, kwargs...) # Set parameter values in SplitODEProblem.
 
-        function make_problem(params; attempt=1, kwargs...)
-            r = Float64[params[k] for k in rs]
-            d = Float64[params[k] for k in ds]
-            b = Float64[params[k] for k in bs]
-            i = Float64[params[k] for k in is]
+    PseudoSpectralProblem(prob, plan, !iszero(boundary_conditions))
+end
 
-            local ϕ, Δϕ
-            if BC
-                ϕ = fϕ(b)
-                Δϕ = fΔϕ(d,b)
-            else
-                ϕ = Δϕ = Matrix{Float64}(undef,n,m)
-            end
+function remake(prob::PseudoSpectralProblem{BC}; p=nothing, u0=nothing, kwargs...)
+    r = Float64[params[k] for k in prob.rs]
+    d = Float64[params[k] for k in prob.ds]
+    b = Float64[params[k] for k in prob.bs]
+    i = Float64[params[k] for k in prob.is]
 
-            w = Matrix{Float64}(undef,n,m) # Allocate working memory for FFTW.
-
-            p = Parameters(w,r,d,ϕ,Δϕ,attempt)
-
-            local u0
-            u0 = fu0(i)
-            BC && (u0 .-= p.ϕ)
+    bc = !isnothing(prob.ϕ)
+    if bc
+        ϕ = fϕ(b)
+        Δϕ = fΔϕ(d,b)
+    else
+        ϕ = Δϕ = Matrix{Float64}(undef,n,m)
+    end
+    w = Matrix{Float64}(undef,n,m) # Allocate working memory for FFTW.
+    p = Parameters(w,r,d,ϕ,Δϕ,attempt)
+    u0 = prob.fu0(i)
+    bc && (u0 .-= p.ϕ)
             plan * u0
             u0 = vec(u0)
             
             update_coefficients!(prob.f.f1.f, nothing, p, nothing) # Set parameter values in diffusion operator.
             remake(prob; p, u0, kwargs...) # Set parameter values in SplitODEProblem.
-        end
-
-        # Function to transform output back to spatial domain.
-        # TODO fix code duplication.
-        function transform(sol, t::Float64)
-            u = sol(t)
-            u = reshape(u,n,m)
-            plan * u
-            BC && (u .+= sol.prob.p.ϕ)
-            u
-        end
-
-        function transform(sol, i::Union{Int,CartesianIndex{1}})
-            @show sol.u
-            u = sol.u[i]
-            u = reshape(u,n,m)
-            plan .* u
-            BC && (u .+= sol.prob.p.ϕ)
-            u
-        end
-
-        transform(sol) = [transform(sol, i) for i in eachindex(sol.u)]
-        
-
-        make_problem, transform
-    end
-    
-    dispatch_bcs(Val(!iszero(boundary_conditions)))
 end
+
+function solve(prob::PseudoSpectralProblem, alg; kwargs...)
+    sol = solve(prob, alg; kwargs)
+    PseudoSpectralSolution(sol, prob.plan, prob.ϕ)
+end
+    
+
+function transform(sol, u)
+    dims = size(sol.plan)
+    u = reshape(u, dims)
+    plan * u
+    if !isnothing(sol.ϕ)
+        u .+= sol.ϕ
+    end
+    u
+end
+
+getindex(sol::PseudoSpectralSolution, i::Union{Int,CartesianIndex{1}}) = transform(sol, sol.sol.u[i])
+(sol::PseudoSpectralSolution)(t) = transform(sol, sol.sol(t))
+get_u(sol::PseudoSpectralSolution) = stack(sol[i] for in in eachindex(sol))
+eachindex(sol::PseudoSpectralSolution) = eachindex(sol.sol)
+
+
 
 "Build function for the reaction component, with `f(v+ϕ) + Δϕ` offset for non-zero-flux BCs."
 function reaction_operator(species, reaction_rates, rs, plan!, ::Val{BC}) where BC
