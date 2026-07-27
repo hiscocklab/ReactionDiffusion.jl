3663a5249802ee73a4f13b5d32672a1bf4871813
Hugh Coleman <h.coleman.25@abdn.ac.uk>
2026-07-27 16:50:53 +0100

    update Models

--- src/Models.jl
+++ src/Models.jl
@@ -8,7 +8,7 @@ export Model, name, species, parameters, reaction_parameters, boundary_parameter
     mol_problem
 
 ## Extended methods
-import ..PseudoSpectral: PseudospectralProblem
+import PseudoSpectral: PseudospectralProblem
 export PseudospectralProblem
 
 import ModelingToolkit: ODESystem
